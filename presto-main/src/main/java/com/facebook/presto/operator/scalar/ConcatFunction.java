cd4ff08a51cffc70ba4e08cc7ae2b5382602f4c0
Sreeni Viswanadha <viswanadha@fb.com>
2021-07-15 13:48:47 -0700

    Improved string concat for code size and performance when it's used for concatenating more than 5 strings.

--- presto-main/src/main/java/com/facebook/presto/operator/scalar/ConcatFunction.java
+++ presto-main/src/main/java/com/facebook/presto/operator/scalar/ConcatFunction.java
@@ -19,9 +19,12 @@ import com.facebook.presto.bytecode.ClassDefinition;
 import com.facebook.presto.bytecode.DynamicClassLoader;
 import com.facebook.presto.bytecode.MethodDefinition;
 import com.facebook.presto.bytecode.Parameter;
+import com.facebook.presto.bytecode.ParameterizedType;
 import com.facebook.presto.bytecode.Scope;
 import com.facebook.presto.bytecode.Variable;
-import com.facebook.presto.bytecode.expression.BytecodeExpression;
+import com.facebook.presto.bytecode.control.IfStatement;
+import com.facebook.presto.bytecode.expression.BytecodeExpressions;
+import com.facebook.presto.bytecode.instruction.LabelNode;
 import com.facebook.presto.common.QualifiedObjectName;
 import com.facebook.presto.common.type.TypeSignature;
 import com.facebook.presto.metadata.BoundVariables;
@@ -50,6 +53,7 @@ import static com.facebook.presto.bytecode.ParameterizedType.type;
 import static com.facebook.presto.bytecode.expression.BytecodeExpressions.add;
 import static com.facebook.presto.bytecode.expression.BytecodeExpressions.constantInt;
 import static com.facebook.presto.bytecode.expression.BytecodeExpressions.invokeStatic;
+import static com.facebook.presto.bytecode.expression.BytecodeExpressions.lessThan;
 import static com.facebook.presto.common.type.VarbinaryType.VARBINARY;
 import static com.facebook.presto.common.type.VarcharType.createUnboundedVarcharType;
 import static com.facebook.presto.metadata.BuiltInTypeAndFunctionNamespaceManager.DEFAULT_NAMESPACE;
@@ -62,7 +66,6 @@ import static com.facebook.presto.util.CompilerUtils.makeClassName;
 import static com.facebook.presto.util.Failures.checkCondition;
 import static com.facebook.presto.util.Reflection.methodHandle;
 import static com.google.common.collect.ImmutableList.toImmutableList;
-import static java.lang.Math.addExact;
 import static java.util.Collections.nCopies;
 
 public final class ConcatFunction
@@ -122,6 +125,48 @@ public final class ConcatFunction
                 methodHandle);
     }
 
+    @UsedByGeneratedCode
+    public static Slice concat(Slice[] slices)
+    {
+        int length = 0;
+        Slice firstNonEmpty = null;
+        for (int i = 0; i < slices.length; i++) {
+            Slice slice = slices[i];
+            if (slice.length() > 0) {
+                length += slice.length();
+                if (length < 0) {
+                    return resultTooLarge();
+                }
+                // Optimize for 0 or 1 non-empty.
+                if (firstNonEmpty == null) {
+                    firstNonEmpty = slice;
+                }
+            }
+        }
+
+        if (length == 0) {
+            // return empty slice
+            return Slices.EMPTY_SLICE;
+        }
+
+        if (length == firstNonEmpty.length()) {
+            // return the only non-empty slice
+            return firstNonEmpty;
+        }
+
+        int position = 0;
+        Slice result = Slices.allocate(length);
+        for (int i = 0; i < slices.length; i++) {
+            Slice slice = slices[i];
+            if (slice.length() > 0) {
+                result.setBytes(position, slice);
+                position += slice.length();
+            }
+        }
+
+        return result;
+    }
+
     private static Class<?> generateConcat(TypeSignature type, int arity)
     {
         checkCondition(arity <= 254, NOT_SUPPORTED, "Too many arguments for string concatenation");
@@ -141,44 +186,50 @@ public final class ConcatFunction
         MethodDefinition method = definition.declareMethod(a(PUBLIC, STATIC), "concat", type(Slice.class), parameters);
         Scope scope = method.getScope();
         BytecodeBlock body = method.getBody();
-
-        Variable length = scope.declareVariable(int.class, "length");
-        body.append(length.set(constantInt(0)));
-
-        for (int i = 0; i < arity; ++i) {
-            body.append(length.set(generateCheckedAdd(length, parameters.get(i).invoke("length", int.class))));
-        }
-
         Variable result = scope.declareVariable(Slice.class, "result");
-        body.append(result.set(invokeStatic(Slices.class, "allocate", Slice.class, length)));
 
-        Variable position = scope.declareVariable(int.class, "position");
-        body.append(position.set(constantInt(0)));
-
-        for (int i = 0; i < arity; ++i) {
-            body.append(result.invoke("setBytes", void.class, position, parameters.get(i)));
-            body.append(position.set(add(position, parameters.get(i).invoke("length", int.class))));
+        if (arity > 10) {
+            Variable slices = scope.declareVariable(Slice[].class, "slices");
+            body.append(slices.set(BytecodeExpressions.newArray(ParameterizedType.type(Slice[].class), parameters)));
+            body.append(invokeStatic(ConcatFunction.class, "concat", Slice.class, slices))
+                    .retObject();
         }
+        else {
+            LabelNode errorLabel = new LabelNode("resultTooLarge");
+            Variable length = scope.declareVariable(int.class, "length");
+            body.append(length.set(constantInt(0)));
 
-        body.getVariable(result)
-                .retObject();
+            for (int i = 0; i < arity; ++i) {
+                body.append(length.set(add(length, parameters.get(i).invoke("length", int.class))));
+                IfStatement overFlowCheck = new IfStatement("if(length < 0) goto end");
+                overFlowCheck.condition(lessThan(length, constantInt(0)))
+                        .ifTrue().gotoLabel(errorLabel);
+                body.append(overFlowCheck);
+            }
+
+            body.append(result.set(invokeStatic(Slices.class, "allocate", Slice.class, length)));
+
+            Variable position = scope.declareVariable(int.class, "position");
+            body.append(position.set(constantInt(0)));
+
+            for (int i = 0; i < arity; ++i) {
+                body.append(result.invoke("setBytes", void.class, position, parameters.get(i)));
+                body.append(position.set(add(position, parameters.get(i).invoke("length", int.class))));
+            }
+            body.getVariable(result)
+                    .retObject();
+
+            body.visitLabel(errorLabel)
+                    .invokeStatic(ConcatFunction.class, "resultTooLarge", Slice.class)
+                    .retObject();
+        }
 
         return defineClass(definition, Object.class, ImmutableMap.of(), new DynamicClassLoader(ConcatFunction.class.getClassLoader()));
     }
 
-    private static BytecodeExpression generateCheckedAdd(BytecodeExpression x, BytecodeExpression y)
-    {
-        return invokeStatic(ConcatFunction.class, "checkedAdd", int.class, x, y);
-    }
-
     @UsedByGeneratedCode
-    public static int checkedAdd(int x, int y)
+    public static Slice resultTooLarge()
     {
-        try {
-            return addExact(x, y);
-        }
-        catch (ArithmeticException e) {
-            throw new PrestoException(INVALID_FUNCTION_ARGUMENT, "Concatenated string is too large");
-        }
+        throw new PrestoException(INVALID_FUNCTION_ARGUMENT, "Concatenated string is too large");
     }
 }
